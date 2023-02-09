b3a53311f283df2046f979c2919ff2ed45fe6aee
serveman <serveman6325@gmail.com>
2023-02-10 02:42:11 +0900

    조건문 연습

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,8 +10,47 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello C#");
-            Console.
+            Solution s = new Solution();
+
+            s.daysUntilExpiration();
+            Console.ReadKey();
+        }
+    }
+
+    internal class Solution
+    {
+        public void daysUntilExpiration()
+        {
+            Random random = new Random();
+            int daysUntilExpiration = random.Next(12);
+            int discountPercentage = 0;
+
+            // Your code goes here
+            if (daysUntilExpiration == 0)
+            {
+                Console.WriteLine("Your subscription has expired.");
+            }
+            else if (daysUntilExpiration <= 1)
+            {
+                Console.WriteLine("Your subscription expires whthin a day!");
+                Console.WriteLine($"Renew now and save {discountPercentage = 20}%!");
+            }
+            else if (daysUntilExpiration <= 5)
+            {
+                Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+                Console.WriteLine($"Renew now and save {discountPercentage = 10}%!");
+            }
+            else if (daysUntilExpiration <= 10)
+            {
+                Console.WriteLine("Your subscription will expire soon. Renew now!");
+            }
+            else
+            {
+
+            }
+
+            Console.WriteLine($"\ndaysUntilExpiration = {daysUntilExpiration}");
+            Console.WriteLine($"discountPercentage = {discountPercentage}%");
         }
     }
 }
