a52a83254c0eb7bb48f3fa3254f537614c5eba83
serveman <serveman6325@gmail.com>
2023-02-11 22:31:39 +0900

    Switch Case 과제

---
+++ ConsoleApp1/NewFolder1/0210/Class4.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.NewFolder1._0210
+{
+    internal class Class4
+    {
+        static void Main(string[] args)
+        {
+            Solution021103 s = new Solution021103();
+
+            s.SwitchCase();
+        }
+    }
+
+    internal class Solution021103
+    {
+        public void SwitchCase()
+        {
+            // SKU = Stock Keeping Unit
+            string sku = "01-MN-L";
+
+            string[] product = sku.Split('-');
+
+            string type = "";
+            string color = "";
+            string size = "";
+
+            switch (product[0])
+            {
+                case "01": type = "Sweat shirt"; break;
+                case "02": type = "T-Shirt"; break;
+                default: type = "Other"; break;
+            }
+
+            switch (product[1])
+            {
+                case "BL": color = "Black"; break;
+                case "MN": color = "Maroon"; break;
+                default: color = "White"; break;
+            }
+
+            switch (product[2])
+            {
+                case "S": size = "Small"; break;
+                case "M": size = "Medium"; break;
+                case "L": size = "Large"; break;
+                default: size = "One Size Fits All"; break;
+            }
+
+            Console.WriteLine($"Product: {size} {color} {type}");
+        }
+    }
+}
