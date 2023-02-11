a04127a3c61825fda95273f3918e8e86ff01c0b9
serveman <serveman6325@gmail.com>
2023-02-11 18:54:39 +0900

    block 범위 학습

---
+++ ConsoleApp1/NewFolder1/0210/Class3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.NewFolder1._0210
+{
+    internal class Class3
+    {
+        static void Main(string[] args)
+        {
+            Solution021102 s = new Solution021102();
+
+            s.isCorrectBlocks();
+        }
+    }
+
+    internal class Solution021102
+    {
+        public void isCorrectBlocks()
+        {
+            int[] numbers = { 4, 8, 15, 16, 23, 42 };
+            int total = 0;
+            bool found = false;
+
+            foreach (int number in numbers)
+            {
+                total += number;
+                if (number == 42) found = true;
+            }
+            if (found) Console.WriteLine("Set contains 42");
+
+            Console.WriteLine($"Total: {total}");
+        }
+    }
+}
